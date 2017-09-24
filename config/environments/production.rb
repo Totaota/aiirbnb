9a94fd40c01d93f6a78d76edb7e1ca01501e75f6
Thomasl <thomas.levot@lamache.org>
2017-09-24 21:10:10 +0000

    Ajour Cloud externe pour heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,8 @@
 Rails.application.configure do
   
   Paperclip::Attachement.default_options.merge!({
-    :storage=>:cloudinary,
-    :path=>':id/:style/:filename'
+    :storage => :cloudinary,
+    :path => ':id/:style/:filename'
   })
   # Settings specified here will take precedence over those in config/application.rb.
 
