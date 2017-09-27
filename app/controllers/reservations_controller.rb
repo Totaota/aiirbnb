ee2d2f64723b4c9f02ac23bce48c79f8f018e802
Thomasl <thomas.levot@lamache.org>
2017-09-27 20:43:16 +0000

    Création controller reservation

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,15 @@
+class ReservationsController < ApplicationController
+    
+    before_action :authenticate_user!
+    
+    def create
+       @reservation = current_user.reservation.create(reservation_params)
+       redirect_to @reservation.room, notice: "Votre réservation à été acceptée"
+    end
+    
+   private
+   def reservation_params
+       params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
+    
+    
+end
