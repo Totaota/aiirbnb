1fc02d550127f02938b29ded317b6122a123d51b
Thomasl <thomas.levot@lamache.org>
2017-10-05 17:53:23 +0000

    Création page voyages

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -3,17 +3,32 @@ class ReservationsController < ApplicationController
     before_action :authenticate_user!
     
     def preload
-        room = Room.find(params[:room_id])
-        today = Date.today
-        reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
-        
-        render json: reservations
-        
+       room = Room.find(params[:room_id])
+       today = Date.today
+       reservations = room.reservations.where("start_date >= ? OR end_date >= ?", today, today)
+       
+       render json: reservations
     end
+    
+    def preview
+        start_date = Date.parse(params[:start_date])
+        end_date = Date.parse(params[:end_date])
+        
+        output = {
+            conflict: is_conflict(start_date, end_date)
+        }
+        
+        render json: output
+    end
+
     
     def create
        @reservation = current_user.reservations.create(reservation_params)
        redirect_to @reservation.room, notice: "Votre réservation à été acceptée"
+    end
+    
+    def your_trips
+       @trips = current_user.reservations
     end
     
    private
@@ -21,5 +36,12 @@ class ReservationsController < ApplicationController
        params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
     end
     
+    private
+        def is_conflict(start_date, end_date)
+            room = Room.find(params[:room_id])
+     
+            check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+            check.size > 0? true : false
+        end
     
 end
