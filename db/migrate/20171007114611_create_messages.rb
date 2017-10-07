d7a6ad10d1a4cdbae1cc0a6758193e554482d3f3
Thomasl <thomas.levot@lamache.org>
2017-10-07 11:59:56 +0000

    Création model conversation et message

---
+++ db/migrate/20171007114611_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.text :content
+      t.references :conversation, index: true, foreign_key: true
+      t.conferences :user
+
+      t.timestamps null: false
+    end
+  end
+end
